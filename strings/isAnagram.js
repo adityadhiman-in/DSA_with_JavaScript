5c5603990d81e61a96be8bfe56f6c1cb4f1b6829
Aditya Dhiman <info@adityadhiman.in>
2024-09-28 19:31:12 +0530

    added truncate string problem and solution

--- strings/isAnagram.js
+++ strings/isAnagram.js
@@ -1,8 +1,1 @@
 // Check if the given string is valid anagram or not
-
-const isAnagram = (str, tar)=>{
-    const strArr = Array.from(str).sort((a,b) => a - b);
-    const tarArr = Array.from(tar).sort((a,b) => a - b);;
-}
-
-console.log(isAnagram("hey", "bey"))
---
+++ strings/truncate.js
@@ -0,0 +1,8 @@
+// Truncate the text 
+// write a function that checks the length of string if more than 10 then trim it and add "..." in it.
+
+const truncate = (str)=>{
+    const validStr = str.length > 10 ? str + " ..." : str;
+    return validStr;
+}
+console.log(truncate("hello there"))
